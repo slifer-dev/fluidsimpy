1af4b2241ed054c0dabedb49da37b5d39825ee07
slifer-dev <lorenzogiardini06@gmail.com>
2024-05-16 21:46:33 +0200

    Scritta la Docstring esplicativa dello scenario

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+The scenario involves a domain filled with a fluid that adheres the Navier-Stokes equations
+
+Domain: the domain that we are dealing with is a closed box withs solid boundaries
+
+The interested Navier-Stokes equation is the momentum one:
+
+Momentum:   ∂u/∂t + (u ⋅ ∇)u = -1/p ∇p + v∇²u + f
+"""
